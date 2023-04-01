a2b43b04215fe60944324487e76a47a39e792ed7
Abdishakur Fifa <85757306+abdishakur1000@users.noreply.github.com>
2023-04-01 07:12:26 +0000

    added api and jobs

--- apps.py
+++ apps.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': "Data Analyst",
+    'location': 'Mogadishu, Somalia',
+    'Salary': '$50,000'
+  },
+  {
+    'id': 2,
+    'title': "Data Scientist",
+    'location': 'Bosaso, Somalia',
+    'Salary': '$80,000'
+  },
+  {
+    'id': 3,
+    'title': "Frontend Engineer",
+    'location': 'Remote, Somalia',
+  },
+  {
+    'id': 4,
+    'title': "Backend Engineer",
+    'location': 'San Francisco, USA',
+    'Salary': '$150,000'
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",
+                         jobs=JOBS,
+                         company_name='Red leaf Petroleum')
+
+
+@app.route("/api/jobs")
+def hello_redleaf():
+  return jsonify(JOBS)
 
 
 # print(__name__)
